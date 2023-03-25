7b76823acc27b49e869f7bef62418b60781d3a19
wibus-wee <1596355173@qq.com>
2023-03-25 15:00:06 +0800

    test: basic test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,24 @@
 import { describe, expect, it } from 'vitest'
+import { djotToHtml } from '../src/handlers/djotToHtml'
+import { markdownToHtml } from '../src/handlers/markdownToHtml'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+const _ = {
+  id: 'test',
+  data: {
+    body: "# H1 \n **bold**",
+  },
+  pattern: '',
+  isEmitter: false,
+}
+
+describe('djot', () => {
+  it('should render raw content correctly', () => {
+    expect(djotToHtml(_.id, _.data, _.pattern, _.isEmitter)).toBe("<section id=\"H1-bold\">\n<h1>H1 \n<strong><strong>bold</strong></strong></h1>\n</section>\n")
+  })
+})
+
+describe("markdown", () => {
+  it("should render raw content correctly", () => {
+    expect(markdownToHtml(_.id, _.data, _.pattern, _.isEmitter)).toBe("<h1 id=\"h1\">H1</h1>\n<p> <strong>bold</strong></p>\n")
   })
 })
